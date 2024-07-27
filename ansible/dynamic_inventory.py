bb22ca051e1bb54135b2999da9e887b65c5bff33
default <ilijakovac1@googlemail.com>
2024-07-27 10:56:04 +0000

    move tasks to subfolder, use custom FQDN, change SSH bastion only for workers

--- ansible/dynamic_inventory.py
+++ ansible/dynamic_inventory.py
@@ -11,22 +11,28 @@ os.chdir('../terraform')
 tf_output = subprocess.check_output(["terraform", "output", "-json"])
 tf_output = json.loads(tf_output)
 
+jumpbox_public_ip = tf_output['jumpbox_public_ip']['value']
+server_public_ip = tf_output['server_public_ip']['value']
+
 # Build the dynamic inventory
 inventory = {
     'jumpbox': {
-        'hosts': [tf_output['jumpbox_public_ip']['value']]
+        'hosts': [jumpbox_public_ip]
     },
     'server': {
-        'hosts': ['10.0.2.10']
+        'hosts': [server_public_ip]
     },
     'worker': {
         'hosts': ['10.0.2.20', '10.0.2.21']
     },
     '_meta': {
         'hostvars': {
-            tf_output['jumpbox_public_ip']['value']: {
-                'ansible_host': tf_output['jumpbox_public_ip']['value']
-            }
+            jumpbox_public_ip: {
+                'ansible_host': jumpbox_public_ip
+            },
+            server_public_ip: {
+                'ansible_host': server_public_ip
+            },
         }
     }
 }
